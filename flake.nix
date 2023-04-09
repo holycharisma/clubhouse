12aef5d73f1d6349cedf9e3ae94eae6254d3bfa9
Holy Charisma <orpheus@lisa.elf-lizard.ts.net>
2023-04-09 13:44:38 -0700

    start to rework build flake ❄️

--- flake.nix
+++ flake.nix
@@ -3,53 +3,44 @@
 
   inputs = {
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
     flake-utils.url  = "github:numtide/flake-utils";     
     
   };
 
-
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, naersk, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        naersk' = pkgs.callPackage naersk {};
       in
       with pkgs;
       {
-        devShells.default = mkShell {
+        devShell = mkShell {
+          nativeBuildInputs = [
+            rustc
+            cargo
+          ];
           buildInputs = [
-            pkg-config
             openssl
             openssl.bin
             pkg-config
-
-            glibc
-            nodejs
-            wasm-pack
-            binaryen
-
-            rust-analyzer
-            (rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" ];
-              targets = [
-                "x86_64-unknown-linux-gnu"  
-                "wasm32-unknown-unknown"
-              ];
-            })
-
           ];
-
-          shellHook = ''
-                mkdir -p .secrets && touch .secrets/.env
-                stat .secrets/jwtRS256.key > /dev/null || ssh-keygen -t rsa -b 4096 -m PEM -f ./.secrets/jwtRS256.key
-                stat .secrets/jwtRS256.key.pub > /dev/null || openssl rsa -in jwtRS256.key -pubout -outform PEM -out ./.secrets/jwtRS256.key.pub
-
-                stat hcc-db/.cargo-nix-local/bin/sea > /dev/null || cargo install sea-orm-cli --version '^0.8.1' --bin sea --root hcc-db/.cargo-nix-local/
-        '';
         };
+
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+          buildInputs = [            
+            openssl
+            openssl.bin
+            pkg-config
+          ];
+        };
+
       }
     );
 
